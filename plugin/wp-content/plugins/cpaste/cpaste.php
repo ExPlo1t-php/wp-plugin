ad4ab180f7009ccaa5471a3e6e0fc5bd17f6ed12
ExPlo1t-php <elabbas.younes.solicode@gmail.com>
2022-04-14 12:36:30 +0000

    first

---
+++ plugin/wp-content/plugins/cpaste/cpaste.php
@@ -0,0 +1,12 @@
+<?php
+    /*
+        Plugin Name: CPaste Duplicateur
+        Description: Permet de dupliquer les articles de WordPress !
+        Version: 0.1
+        Author: CPaste .inc
+    */
+
+    if ( ! defined( 'ABSPATH' ) ) 
+        exit;
+
+    require plugin_dir_path(__FILE__), 'vendor/autoload.php';
